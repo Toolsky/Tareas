ae6b0db85006b879341741e3241c93d6f589f8cc
postgres <94930131+Bastiancito@users.noreply.github.com>
2023-06-12 22:15:24 -0400

    arreglao
    arreglao

--- src/controllers/ApiController.js
+++ src/controllers/ApiController.js
@@ -22,16 +22,29 @@ const mas_fuertes= async (req,res) => {
 
 const mas_karts = async (req, res) => {
     try {
-        const id_reino = Number(req.params.id_reino)
-        const hab_reino = await prisma.personaje_habita_reino.count({
-            where: {id_reino}
-        })
-        res.json(hab_reino)
+      const pj = await prisma.personajes.findMany({
+        include: { karts: true },
+      });
+      let pj_maxkarts;
+      let contador = 0;
+  
+      for (const personaje of pj) {
+        const cant_karts = personaje.karts.length;
+        if (cant_karts > contador) {
+          pj_maxkarts = personaje;
+          contador = cant_karts;
+        }
+      }
+  
+      const cantidad_karts = pj_maxkarts.karts.length;
+      res.json({
+        nombre: pj_maxkarts.nombre,
+        cantidad_karts: cantidad_karts,
+      });
     } catch (error) {
-        res.status(400).send("No se pudo encontrar la cantidad de habitantes del reino")
+      res.status(400).send("Error al obtener el personaje con más karts");
     }
-}
-
+  };
 
 const gobernantes = async (req,res) => {
     try {
